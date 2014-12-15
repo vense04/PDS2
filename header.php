a4db2fd48ca06d256baa8cc64781fa445b8afef3
rodrigofonsecaandradejunior <rodrigofonsecaandradejunior@192.168.0.13>
2014-12-15 09:57:22 -0200

    Ajustes

--- header.php
+++ header.php
@@ -59,7 +59,7 @@
 					<li class="dropdown"><a href="#" class="dropdown-toggle"
 						data-toggle="dropdown" role="button" aria-expanded="false"> <img
 							class="profile-img foto"
-							src="assets/img/<?= $avatar ?>" style="max-width: 100px; max-height: 41px" /> <span class="caret"></span></a>
+							src="assets/avatar/<?= $avatar ?>" style="max-width: 100px; max-height: 41px" /> <span class="caret"></span></a>
 						<ul class="dropdown-menu" role="menu">
 							<li><a href="#">Meus Cursos</a></li>
 							<li class="divider"></li>
